fdd5d8904e57a854b6a5ee3171b5452d80d8c176
Takeda Chiharu <takeda123pc@gmail.com>
2020-12-15 07:51:44 +0900

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-
+import Counter from './Counter'
 interface AppProps {
   message?: string;
 }
 
 //コンポーネントに対して型指定してあげないとまずい。
 const App: React.FunctionComponent<AppProps> = ({ message }) => {
-  return <div>{message}</div>;
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
 };
 
 //propsの初期値設定できる。
